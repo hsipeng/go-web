e4c8f1082139a0e6749883b85de05524f1a771f4
lirawx <lira_wx@outlook.com>
2020-12-17 16:05:33 +0800

    feat: docker file build

--- main.go
+++ main.go
@@ -2,48 +2,20 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-
-	"lirawx.cn/go-web/config"
-	"lirawx.cn/go-web/db"
-	"lirawx.cn/go-web/models"
-	"lirawx.cn/go-web/routers"
+	"net/http"
 )
 
-// @title go-web API 文档
-// @version 1.0
-// @description API 文档
-// @termsOfService http://swagger.io/terms/
-
-// @contact.name 这里写联系人信息
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host localhost:8080
-// @BasePath /
 func main() {
-	r := routers.SetupRouter()
-	conf := config.ReadConfig()
-	fmt.Printf("config 配置加载 --- %v\n", conf)
-	if err := db.InitMySQL(conf); err != nil {
-		fmt.Printf("init mysql failed, err:%v\n", err)
-		return
+	http.HandleFunc("/", hello)
+	server := &http.Server{
+		Addr: ":8888",
 	}
-	// 模型绑定
-	db.DB.AutoMigrate(&models.Todo{})
-
-	// redis
-
-	if err := db.InitRedisClient(conf.Redis); err != nil {
-		fmt.Printf("init redis failed, err:%v\n", err)
-		return
+	fmt.Println("server startup...")
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Printf("server startup failed, err:%v\n", err)
 	}
+}
 
-	// Run
-	if err := r.Run(":" + strconv.Itoa(conf.Port)); err != nil {
-		fmt.Printf("startup service failed, err:%v\n", err)
-	}
+func hello(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("hello liwenzhou.com!"))
 }
